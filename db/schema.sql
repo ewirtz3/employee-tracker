534a9fa688ba16123c6154358c741564949c9ad1
Emily Wirtz <ewirtz3@gmail.com>
2020-06-07 21:39:23 -0500

    adding foreign keys to schemas

--- db/schema.sql
+++ db/schema.sql
@@ -12,6 +12,7 @@ CREATE TABLE role (
     title VARCHAR(30) NOT NULL,
     salary DECIMAL (10, 2) NOT NULL,
     department_id INTEGER NOT NULL
+    FOREIGN KEY (department_id) REFERENCES department(id);
 );
 
 CREATE TABLE employee (
@@ -19,5 +20,6 @@ CREATE TABLE employee (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER NOT NULL,
-    manager_id INTEGER
+    manager_id INTEGER,
+    FOREIGN KEY (role_id) REFERENCES role(id);
 );
