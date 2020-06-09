83fb6dfe63f67fcef2f13f914fad5cd54506ffd2
Emily Wirtz <ewirtz3@gmail.com>
2020-06-09 18:20:10 -0500

    viewByManager working

--- index.js
+++ index.js
@@ -90,44 +90,33 @@ async function viewByDepartment() {
     });
 }
 
-async function viewByManager() {
-  const managers = await connection.query(
+function viewByManager() {
+  connection.query(
     "SELECT employee.id, CONCAT(employee.first_name, ' ', employee.last_name) as name FROM employee WHERE manager_id IS NULL",
-    (err, res) => {
+    async (err, res) => {
       if (err) throw err;
-      console.log(res[1]);
-      return res;
+      const manager_choices = res.map((manager) => ({
+        name: manager.name,
+        value: manager.id,
+      }));
+
+      const { manager_id } = await inquirer.prompt({
+        name: "manager_id",
+        type: "list",
+        message: "Which manager's team would you like to view?",
+        choices: manager_choices,
+      });
+
+      connection.query(
+        "SELECT employee.id, CONCAT(employee.first_name, ' ', employee.last_name) as name FROM employee WHERE manager_id = ?",
+        [manager_id],
+        (err, res) => {
+          if (err) throw err;
+          console.table(res);
+        }
+      );
     }
   );
-
-  const manager_choices = function (managers) {
-    for (i = 0; i < managers.length; i++) {
-      id: managers[i].id;
-      name: managers[i].name;
-    }
-  };
-  const manager_id = await inquirer
-    .prompt({
-      name: "manager_id",
-      type: "list",
-      message: "Which manager's team would you like to view?",
-      choices: manager_choices,
-    })
-    .then((manager_id) => {
-      console.log(manager_id);
-      return manager_id.id;
-    });
-
-  const direct_reports = await ((manager_id) => {
-    connection.query(
-      "SELECT CONCAT(employee.first_name, '', employee.last_name), role.title, department.name AS department LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id WHERE department.id = ?",
-      manager_id,
-      (err, res) => {
-        if (err) throw err;
-        console.table(res);
-      }
-    );
-  });
 }
 
 function addEmployee() {}
