177d8652dfadba2e5ccb8d3d59a2c3946cfcbb74
Emily Wirtz <ewirtz3@gmail.com>
2020-06-15 12:49:43 -0500

    add employee working

--- index.js
+++ index.js
@@ -119,7 +119,64 @@ function viewByManager() {
   );
 }
 
-function addEmployee() {}
+async function addEmployee() {
+  connection.query(
+    "SELECT DISTINCT role.title, role.id FROM role",
+    async (err, res) => {
+      if (err) throw err;
+      const roleChoices = res.map(({ id, title }) => ({
+        name: title,
+        value: id,
+      }));
+      const employee = await inquirer.prompt([
+        {
+          name: "first_name",
+          message: "What is the employee's first name?",
+        },
+        {
+          name: "last_name",
+          message: "What is the employee's last name?",
+        },
+      ]);
+
+      const { roleId } = await inquirer.prompt({
+        type: "list",
+        name: "roleId",
+        message: "What is the employee's role?",
+        choices: roleChoices,
+      });
+
+      employee.role_id = roleId;
+
+      const managers = await connection.query(
+        "SELECT employee.id, CONCAT(employee.first_name, ' ', employee.last_name) as name FROM employee WHERE manager_id IS NULL",
+        async (err, res) => {
+          if (err) throw err;
+          const manager_choices = res.map((manager) => ({
+            name: manager.name,
+            value: manager.id,
+          }));
+          const { manager_id } = await inquirer.prompt({
+            name: "manager_id",
+            type: "list",
+            message: "Who is the employee's manager?",
+            choices: manager_choices,
+          });
+
+          employee.manager_id = manager_id;
+        }
+      );
+
+      const newEmployee = await connection.query("INSERT INTO employee SET ?", [
+        employee,
+      ]);
+
+      res.json(newEmployee);
+
+      questionOne();
+    }
+  );
+}
 
 function removeEmployee() {}
 
